5db149cec9db2d6221118fee1abab5fef3a8e22b
Antobio17 <antoniojr997@gmail.com>
2021-10-02 15:17:35 +0200

    Se continua trabajando en la funcionalidad -d. Se empieza a trabajar en la funcionalidad -l.

--- Practica1/ParseDirectoryFiles.java
+++ Practica1/ParseDirectoryFiles.java
@@ -1,6 +1,14 @@
 import java.io.File;
+import java.io.InputStream;
+import java.io.FileInputStream;
 import org.apache.tika.Tika;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.AutoDetectParser;
+import org.xml.sax.ContentHandler;
+import org.apache.tika.sax.LinkContentHandler;
+import org.apache.tika.sax.BodyContentHandler;
+import org.apache.tika.sax.TeeContentHandler;
 import org.apache.tika.language.LanguageIdentifier;
 
 public class ParseDirectoryFiles {
@@ -24,18 +32,50 @@ public class ParseDirectoryFiles {
       System.exit(-1);
     }
 
+    // Creaamos las instancias para las funcionalidades con la configuracion por defecto
+    Tika tika = new Tika();
+    Metadata metadata = new Metadata();
+    ParseContext parseContext = new ParseContext();
+    AutoDetectParser parser = new AutoDetectParser();
+    LinkContentHandler linkHandler = new LinkContentHandler();
+    ContentHandler contentHandler = new BodyContentHandler();
+    TeeContentHandler teeHandler = new TeeContentHandler(linkHandler, contentHandler);
+
     files = directory.listFiles();
     // Elegimos según opción introducida
     switch (option){
       case "-d":
         for (File file : files) {
-          System.out.println(file.getName());
+          InputStream is = new FileInputStream(file);
+
+          parser.parse(is, teeHandler, metadata, parseContext);
+
+          System.out.println("\nNombre: " + file.getName());
+
+          String contentType = metadata.get("Content-Type");
+          String[] dataContentType = contentType.split(";");
+          System.out.println("Tipo: " + dataContentType[0]);
+
+          System.out.println("Codificación: " + metadata.get("Content-Encoding"));
+          
+          LanguageIdentifier identifier = new LanguageIdentifier(contentHandler.toString());
+          System.out.println("Idioma: " + identifier.getLanguage());
         }
         break;
 
       case "-l":
         for (File file : files) {
-            
+          InputStream is = new FileInputStream(file);
+
+          parser.parse(is, teeHandler, metadata, parseContext);
+
+          System.out.println("\nNombre: " + file.getName());     
+          
+          if (!linkHandler.getLinks().isEmpty()) {
+            System.out.println("\nLinks: " + linkHandler.getLinks());        
+          } else {
+            System.out.println("\nLinks: El archivo no tiene links.");        
+          }
         }
         break;  
 
