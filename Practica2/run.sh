aacef451a3ffa41162276835f8b91ae42dcabf57
Antobio17 <antoniojr997@gmail.com>
2021-10-17 12:15:43 +0200

    Se añade clase Document y libreria tika para su compilación y ejecución.

--- Practica2/run.sh
+++ Practica2/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-javac -cp lucene-core-7.1.0.jar:lucene-analyzers-common-7.1.0.jar src/*.java
-java -cp lucene-core-7.1.0.jar:.:lucene-analyzers-common-7.1.0.jar:. src.TextAnalyzer $*
+javac -cp tika-app-1.24.1.jar:lucene-core-7.1.0.jar:lucene-analyzers-common-7.1.0.jar src/*.java
+java -cp tika-app-1.24.1.jar:.:lucene-core-7.1.0.jar:.:lucene-analyzers-common-7.1.0.jar:. src.TextAnalyzer $*
 
 rm src/*.class
---
+++ Practica2/src/Document.java
@@ -0,0 +1,263 @@
+package src;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.language.LanguageIdentifier;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.sax.Link;
+import org.apache.tika.sax.TeeContentHandler;
+import org.apache.tika.sax.LinkContentHandler;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.ContentHandler;
+
+public class Document {
+    
+    /******************************* PROPERTIES ******************************/
+
+    private String name;
+    private String type;
+    private String coding;
+    private String language;
+    private String body;
+
+    Metadata metadata = new Metadata();
+    ParseContext parseContext = new ParseContext();
+    AutoDetectParser parser = new AutoDetectParser();
+    LinkContentHandler linkHandler = new LinkContentHandler();
+    ContentHandler contentHandler = new BodyContentHandler(-1);  // El valor -1 elimina el límite de caracteres para recuperar del documento
+    TeeContentHandler teeHandler = new TeeContentHandler(linkHandler, contentHandler);
+
+    /******************************* CONSTRUCTS ******************************/
+
+    /**
+     * Document construct.
+     * 
+     * @param name String: Nombre del documento.
+     */
+    public Document(File file) throws FileNotFoundException, 
+        IOException, SAXException, TikaException
+    {
+        InputStream is = new FileInputStream(file);
+        this.parser.parse(is, this.teeHandler, this.metadata, this.parseContext);
+
+        String contentType = this.metadata.get("Content-Type");
+        String[] type = contentType.split(";");
+
+        String body = contentHandler.toString();
+
+        LanguageIdentifier identifier = new LanguageIdentifier(body);
+
+        this.setName(file.getName())
+            .setType(type[0])
+            .setCoding(this.metadata.get("Content-Encoding"))
+            .setLanguage(identifier.getLanguage())
+            .setBody(body);
+    }
+
+    /**
+     * Document construct.
+     * 
+     * @param name String: Nombre del documento.
+     * @param type String: Tipo de documento.
+     * @param coding String: Codificación del documento.
+     * @param language String: Lenguaje del documento.
+     * @param body String: Cuerpo del documento.
+     */
+    public Document(String name, String type, String coding, String language,
+        String body)
+    {
+        this.setName(name)
+            .setType(type)
+            .setCoding(coding)
+            .setLanguage(language)
+            .setBody(body);
+    }
+
+    /*************************** GETTER AND SETTER ***************************/
+    
+    /**
+     * Obtiene la propiedad Name de la clase.
+     * 
+     * @return String
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    /**
+     * Establece la propiedad Name de la clase.
+     * 
+     * @param name String: name a establecer en la clase.
+     *
+     * @return this
+     */
+    public Document setName(String name)
+    {
+        this.name = name;
+
+        return this;
+    }
+
+    /**
+     * Obtiene la propiedad Type de la clase.
+     * 
+     * @return String
+     */
+    public String getType()
+    {
+        return this.type;
+    }
+
+    /**
+     * Establece la propiedad Type de la clase.
+     * 
+     * @param type String: type a establecer en la clase.
+     *
+     * @return this
+     */
+    public Document setType(String type)
+    {
+        this.type = type;
+
+        return this;
+    }
+
+    /**
+     * Obtiene la propiedad Coding de la clase.
+     * 
+     * @return String
+     */
+    public String getCoding()
+    {
+        return this.coding;
+    }
+
+    /**
+     * Establece la propiedad Coding de la clase.
+     * 
+     * @param coding String: coding a establecer en la clase.
+     *
+     * @return this
+     */
+    public Document setCoding(String coding)
+    {
+        this.coding = coding;
+
+        return this;
+    }
+
+    /**
+     * Obtiene la propiedad Language de la clase.
+     * 
+     * @return String
+     */
+    public String getLanguage()
+    {
+        return this.language;
+    }
+
+    /**
+     * Establece la propiedad Language de la clase.
+     * 
+     * @param language String: language a establecer en la clase.
+     *
+     * @return this
+     */
+    public Document setLanguage(String language)
+    {
+        this.language = language;
+
+        return this;
+    }
+
+    /**
+     * Obtiene la propiedad Body de la clase.
+     * 
+     * @return String
+     */
+    public String getBody()
+    {
+        return this.body;
+    }
+
+    /**
+     * Establece la propiedad Body de la clase.
+     * 
+     * @param body String: body a establecer en la clase.
+     *
+     * @return this
+     */
+    public Document setBody(String body)
+    {
+        this.body = body;
+
+        return this;
+    }
+
+    /***************************** PUBLIC METHODS ****************************/
+
+    /**
+     * Obtiene los todos los links del contenido del documento.
+     * 
+     * @return List<Link>
+     */
+    public List<Link> getLinks()
+    {
+        return this.linkHandler.getLinks();
+    }
+
+    /**
+     * Método para imprimir por pantalla los links del cuerpo del documento.
+     */
+    public void printLinks()
+    {
+        if (!this.getLinks().isEmpty()) {
+            System.out.println("Links: ");     
+            for (Link link : this.getLinks()) {
+                System.out.println("  " + link);  
+            }
+        } else {
+            System.out.println("Links:\n  El documento no tiene links.");        
+        }
+    }
+
+    /**
+     * Método para obtener las ocurrencias de las distintas palabras del documento.
+     * 
+     * @return HashMap<String, Integer>
+     */
+    public HashMap<String, Integer> getOccurrences()
+    {
+        HashMap<String, Integer> occurrences = new HashMap <String, Integer> ();
+        
+        // Limpiamos el body del documento y separamos por espacios y saltos de linea.
+        String[] words = this.getBody().replaceAll("[\\(\\)¡!¿?→,.:;\\-—\"«»“”0123456789]", "").split("\\s+");
+        for (String word : words) {
+            word = word.toLowerCase();
+            int numOccurrences = 
+                occurrences.containsKey(word) ?
+                    occurrences.get(word) + 1 : 1;
+
+            occurrences.put(word, numOccurrences);
+        }
+
+        return occurrences;
+    }
+    /***************************** PRIVARE METHODS ***************************/
+
+    /***************************** STATIC METHODS ****************************/
+}
