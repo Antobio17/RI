177c99472b106f371c4f1c2763cb7d45e9db6389
Antobio17 <antoniojr997@gmail.com>
2021-10-26 20:10:04 +0200

    Creación de la clase indexadora de documentos.

---
+++ Practica3/src/Indexer.java
@@ -0,0 +1,10 @@
+import org.apache.lucene.document.Field;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+
+public class Indexer {
+    
+}
