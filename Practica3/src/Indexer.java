27b748e2f845a20d27e687325a82feac35b86bce
Antobio17 <antoniojr997@gmail.com>
2021-11-01 17:57:50 +0100

    Se trabaja para crear un PerFieldAnalyzerWrapper.

--- Practica3/src/Indexer.java
+++ Practica3/src/Indexer.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 
 import java.nio.file.Paths;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.IntPoint;  // Con esta clase no se almacena el valor, es mas rápido que IntField
@@ -18,7 +21,11 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.analysis.core.KeywordAnalyzer;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+
 
 import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
 
@@ -32,13 +39,13 @@ public class Indexer {
      * 
      * @throws IOException
      */
-    public Indexer() throws IOException
+    public Indexer(String[] headers) throws IOException
     {
         FSDirectory directory = FSDirectory.open(Paths.get("./index"));
 
-        Analyzer standardAnalyzer = new StandardAnalyzer();
-        IndexWriterConfig config = new IndexWriterConfig(standardAnalyzer);
-        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+        // Analyzer standardAnalyzer = new StandardAnalyzer();
+        IndexWriterConfig config = new IndexWriterConfig(this.getPerFieldAnalyzer(headers));
+        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
         
         writer = new IndexWriter(directory, config);
     }
@@ -56,99 +63,43 @@ public class Indexer {
         {
             if (!data[i].equals("")){
                 switch (headers[i].toLowerCase()) {
-                    case "authors":
-                        document.add(new TextField(headers[i], data[i], Field.Store.YES));
-                        break;
-                    case "author(s) id":
-                        // String[] idsStr = data[i].split(";");
-                        // long[] ids = new long[idsStr.length];
-                        // for (int j = 0; j < idsStr.length; j++) {
-                            // ids[j] = Long.parseLong(idsStr[j]);
-                        // }
-                        // document.add(new LongPoint(headers[i], ids));
-                        document.add(new StringField(headers[i], data[i], Field.Store.YES));
-                        break;
-                    case "title":
-                        document.add(new StringField(headers[i], data[i], Field.Store.YES));
-                        break;
+                    
                     case "year":
-                        int[] year = new int[1];
-                        year[0] = Integer.parseInt(data[i]);
-                        document.add(new IntPoint(headers[i], year));
-                        break;
-                    case "source title":
-                        document.add(new StringField(headers[i], data[i], Field.Store.YES));
-                        break;
-                    case "volume":
-                        document.add(new StringField(headers[i], data[i], Field.Store.YES));
-                        break;
-                    case "issue":
-                        document.add(new StringField(headers[i], data[i], Field.Store.YES));
-                        break;
-                    case "art. no.":
-                        document.add(new StringField(headers[i], data[i], Field.Store.YES));
-                        break;
-                    case "page start":
-                        document.add(new StringField(headers[i], data[i], Field.Store.YES));
-                        break;
-                    case "page end":
-                        document.add(new StringField(headers[i], data[i], Field.Store.YES));
-                        break;
                     case "page count":
-                        int[] pageCount = new int[1];
-                        pageCount[0] = Integer.parseInt(data[i]);
-                        document.add(new IntPoint(headers[i], pageCount));
+                        int[] intField = new int[1];
+                        intField[0] = Integer.parseInt(data[i]);
+                        document.add(new IntPoint(headers[i], intField));
                         break;
+
+                    case "author(s) id":
+                    case "title":
+                    case "source title":
+                    case "volume":
+                    case "issue":
+                    case "art. no.":
+                    case "page start":
+                    case "page end":
                     case "cited by":
-                        // System.out.println(data[i]);
-                        // System.exit(0);
-                        document.add(new StringField(headers[i], data[i], Field.Store.YES));
-                        break;
                     case "doi":
-                        document.add(new StringField(headers[i], data[i], Field.Store.YES));
-                        break;
                     case "link":
-                        document.add(new StringField(headers[i], data[i], Field.Store.YES));
-                        break;
-                    case "affiliations":
-                        document.add(new TextField(headers[i], data[i], Field.Store.YES));
-                        break;
-                    case "authors with affiliations":
-                        // Analizador propio?
-                        // System.out.println(data[i]);
-                        // System.exit(0);
-                        document.add(new TextField(headers[i], data[i], Field.Store.YES));
-                        break;
-                    case "abstract":
-                        document.add(new TextField(headers[i], data[i], Field.Store.YES));
-                        break;
-                    case "author keywords":
-                        // Separador de ;
-                        document.add(new TextField(headers[i], data[i], Field.Store.YES));
-                        break;
-                    case "index keywords":
-                        // separador por ;
-                        document.add(new TextField(headers[i], data[i], Field.Store.YES));
-                        break;
                     case "document type":
-                        document.add(new StringField(headers[i], data[i], Field.Store.YES));
-                        break;
                     case "publication stage":
-                        document.add(new StringField(headers[i], data[i], Field.Store.YES));
-                        break;
                     case "open access":
-                        document.add(new StringField(headers[i], data[i], Field.Store.YES));
-                        break;
-                    case "source":
-                        document.add(new TextField(headers[i], data[i], Field.Store.YES));
-                        break;
                     case "eid":
                         document.add(new StringField(headers[i], data[i], Field.Store.YES));
                         break;
-
+                    
+                    case "authors":
+                    case "affiliations":
+                    case "authors with affiliations":
+                    case "abstract":
+                    case "author keywords":
+                    case "index keywords":
+                    case "source":
                     default:
                         document.add(new TextField(headers[i], data[i], Field.Store.YES));
                         break;
+
                 }
             }
         }
@@ -180,60 +131,49 @@ public class Indexer {
      */
     protected PerFieldAnalyzerWrapper getPerFieldAnalyzer(String[] headers)
     {
+        Map<String, Analyzer> analyzerPerField = new HashMap<String, Analyzer>();
         for(String header : headers)
         {
             switch (header.toLowerCase()) {
                 case "authors":
-                    break;
-                case "author(s) id":
-                    break;
-                case "title":
-                    break;
-                case "year":
-                    break;
                 case "source title":
-                    break;
-                case "volume":
-                    break;
                 case "issue":
-                    break;
-                case "art. no.":
-                    break;
-                case "page start":
-                    break;
-                case "page end":
-                    break;
-                case "page count":
-                    break;
                 case "cited by":
-                    break;
-                case "doi":
-                    break;
-                case "link":
-                    break;
-                case "affiliations":
-                    break;
-                case "authors with affiliations":
-                    break;
-                case "abstract":
-                    break;
                 case "author keywords":
-                    break;
                 case "index keywords":
+                    analyzerPerField.put(header, new StandardAnalyzer());
                     break;
+                
+                case "title":
+                case "affiliations":
+                case "authors with affiliations":
+                case "abstract":
+                    analyzerPerField.put(header, new EnglishAnalyzer());
+                    break;
+                
+                case "author(s) id":
+                case "year":
+                case "volume":
+                case "art. no.":
+                case "page start":
+                case "page end":
+                case "page count":
+                case "doi":
+                case "link":
                 case "document type":
-                    break;
                 case "publication stage":
-                    break;
                 case "open access":
-                    break;
                 case "source":
-                    break;
                 case "eid":
+                    analyzerPerField.put(header, new KeywordAnalyzer());
                     break;
+                
                 default:
+                    analyzerPerField.put(header, new WhitespaceAnalyzer());
                     break;
             }
         }
+
+        return new PerFieldAnalyzerWrapper(new WhitespaceAnalyzer(), analyzerPerField);
     }
 }
